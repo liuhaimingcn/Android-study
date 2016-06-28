32b05ddb4fed0ee6612a6d1cb1445e330ce040ea
刘海铭 <hymanliu@raventech.com>
2016-06-28 14:49:33 +0800

    hello world

---
+++ app/src/main/java/com/liuhaiming/demo1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.liuhaiming.demo1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * @author Created by liuhaiming on 16/6/28.
+ */
+public class MainActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
